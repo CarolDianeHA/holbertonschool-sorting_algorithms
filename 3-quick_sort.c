fbc5d78e179ed400f3ff6de87fc2f7effa1dcded
Melady1980 <carol.d.hernandez@outlook.com>
2023-01-09 19:37:11 +0000

    Function that sorts an array of integers in ascending order using the Quick sort algorithm.

---
+++ 3-quick_sort.c
@@ -0,0 +1,82 @@
+#include "sort.h"
+
+/**
+ * swap - swaps two values
+ * @array: data sort input
+ * @firts: first
+ * @second: second
+ * @size: size
+ * Return: No Return
+ */
+void swap(int *array, int firts, int second, int size)
+{
+	int tmp;
+
+	if (array[firts] != array[second])
+	{
+		tmp = array[firts];
+		array[firts] = array[second];
+		array[second] = tmp;
+		print_array(array, size);
+	}
+}
+
+/**
+ * part - part to a pivot
+ * @array: data sort input
+ * @left: left
+ * @right: right
+ * @size: size input
+ * Return: New pivote
+ */
+int part(int *array, int left, int right, size_t size)
+{
+	int i = left, j, pivot  = array[right];
+
+	for (j = left; j <= right; j++)
+	{
+		if (array[j] < pivot)
+		{
+			swap(array, i, j, size);
+			i++;
+		}
+	}
+	swap(array, i, right, size);
+
+	return (i);
+}
+
+/**
+ * myquicksort -  quick sort algorithm
+ * @array: data to sort
+ * @left: left pivote
+ * @right: right pivote
+ * @size: size input
+ * Return: No Return
+ */
+void myquicksort(int *array, int left, int right, size_t size)
+{
+	int pivote;
+
+	if (left < right)
+	{
+		pivote = part(array, left, right, size);
+		myquicksort(array, left, pivote - 1, size);
+		myquicksort(array, pivote + 1, right, size);
+	}
+}
+
+/**
+ * quick_sort - Function that sorts an array of integers
+ * in ascending order using the quick sort algorithm.
+ * @array: array pointer
+ * @size: size of the array
+ * Return: nothing
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (size < 2)
+		return;
+
+	myquicksort(array, 0, size - 1, size);
+}
